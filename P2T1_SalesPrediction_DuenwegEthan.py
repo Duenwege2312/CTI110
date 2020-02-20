eb8579eed3c1d8486d29ba4dac5da0d366095a16
Duenwege2312 <61291304+Duenwege2312@users.noreply.github.com>
2020-02-20 13:49:11 -0500

    Sales Prediction
    CTI Assignment

---
+++ P2T1_SalesPrediction_DuenwegEthan.py
@@ -0,0 +1,8 @@
+# This is a program to calculate the profit from your total sales
+# 2/13/2020
+# CTI-110 P2T1 - Sales Prediction
+# Ethan Duenweg
+#
+total_sales = float ( input ('Enter the projected sales: ') )
+profit = total_sales * 0.23
+print ('The profit is $', format (profit, ',.2f'))
